8a1118631fbc6aa1d7d4c72d5e03e8884bf4fa1d
Ayush Kumar <engiworks2016@gmail.com>
2021-10-17 22:24:27 +0530

    Add header.php file

---
+++ admin/includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/vendor/css/font-awesome.min.css">
+  <link rel="icon" href="../assets/img/favicon.png" type="image/png">
+  <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="assets/scss/main.css">
