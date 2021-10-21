bc4c96297378a3110a4a9e14f7d8cbe4c3cf7b2c
Ayush Kumar <engiworks2016@gmail.com>
2021-10-21 17:37:16 +0530

    add admin dashboard

---
+++ admin/admin-dashboard.php
@@ -0,0 +1,13 @@
+
+
+<?php include "includes/header.php" ?>
+
+<title>GammaFlight &dash; Admin Dashboard</title>
+</head>
+<body>
+  <div class="ad__wrapper">
+
+  </div>
+
+</body>
+</html>
--- admin/dashboard.php
+++ admin/dashboard.php
@@ -8,16 +8,16 @@
 
 <?php include "includes/header.php"; ?>
 
-<title>GammaFlight &dash; Admin Dashboard</title>
+  <title>GammaFlight &dash; Admin Dashboard</title>
 </head>
 <body>
   <div class="ad__wrapper">
     <div class="ad__sidebar">
-      
+      Sidebar
     </div>
 
     <div class="ad__main">
-        Main content
+        <h1>Main</h1>Main content
     </div>
   </div>
 </body>
